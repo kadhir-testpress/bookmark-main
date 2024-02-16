3e238c00ca62fd7b768312a9f385426b96e1431b
kadhir <kadhir@kadhir.com>
2024-02-16 17:48:27 +0530

    Implement User account registration

--- account/views.py
+++ account/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+from .models import Profile
 
 def user_login(request):
     if request.method == 'POST':
@@ -33,3 +34,26 @@ def user_logout(request):
 @login_required(login_url='/account/login/')
 def dashboard(request):
     return render(request, 'account/dashboard.html')
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new user object but avoid saving it yet
+            new_user = user_form.save(commit=False)
+            # Set the chosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+            # Create the user profile
+            Profile.objects.create(user=new_user)
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
+        
