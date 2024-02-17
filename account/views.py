cc4d56928dd719bfb18ce48237020136d26fcb8f
kadhir <kadhir@kadhir.com>
2024-02-17 12:42:36 +0530

    Refactor some changes fix CSS functionality

--- account/views.py
+++ account/views.py
@@ -1,40 +1,41 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
 from django.contrib import messages
+from .models import Profile
+from .forms import LoginForm, UserRegistrationForm, \
+                   UserEditForm, ProfileEditForm
+
 
 def user_login(request):
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
-            cd = form.cleaned_data 
+            cd = form.cleaned_data
             user = authenticate(request,
                                 username=cd['username'],
                                 password=cd['password'])
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfully')
+                    return HttpResponse('Authenticated '\
+                                        'successfully')
                 else:
-                    return HttpResponse('Account disabled')
+                    return HttpResponse('Disabled account')
             else:
                 return HttpResponse('Invalid login')
     else:
         form = LoginForm()
-    return render(request, 'account/login.html', {'form':form})
+    return render(request, 'account/login.html', {'form': form})
 
-def user_logout(request):
-    # Logout the user
-    logout(request)
-    # Redirect to a template or another view after logout
-    return render(request, 'account/logged_out.html')
 
-@login_required(login_url='/account/login/')
+@login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html')
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
+
 
 def register(request):
     if request.method == 'POST':
@@ -57,7 +58,8 @@ def register(request):
     return render(request,
                   'account/register.html',
                   {'user_form': user_form})
-        
+
+
 @login_required
 def edit(request):
     if request.method == 'POST':
@@ -80,4 +82,3 @@ def edit(request):
                   'account/edit.html',
                   {'user_form': user_form,
                    'profile_form': profile_form})
-
