0da53d3300549f71ee1321befe63ecd0fe545ebb
kadhir <kadhir@kadhir.com>
2024-02-19 13:02:13 +0530

    Implement follow

--- account/views.py
+++ account/views.py
@@ -1,13 +1,12 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib import messages
-from .models import Profile
-from .forms import LoginForm, UserRegistrationForm, \
-                   UserEditForm, ProfileEditForm
-
+from .models import Profile, Contact
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from django.views.decorators.http import require_POST
 
 def user_login(request):
     if request.method == 'POST':
@@ -99,3 +98,27 @@ def user_detail(request,username):
     return render(request, 
                   'account/user/detail.html',
                   { 'section':'people','user':user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user
+                )
+            else:
+                Contact.objects.filter(
+                    user_from=request.user,
+                    uer_to=user
+                ).delete()
+            return JsonResponse({'status':'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status':'error'})
+    return JsonResponse({'status':'error'})
