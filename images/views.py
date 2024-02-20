902d0b2ac4feafea1d93c3cab273893b61cc5304
kadhir <kadhir@kadhir.com>
2024-02-20 11:21:48 +0530

    Integrate Redis for Image View Counter

--- images/views.py
+++ images/views.py
@@ -7,6 +7,10 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from actions.utils import create_action
+import redis
+from django.conf import settings
+
+r = redis.Redis(host=settings.REDIS_HOST,port=settings.REDIS_PORT,db=settings.REDIS_DB)
 @login_required 
 def image_create(request):
     if request.method == 'POST':
@@ -26,7 +30,8 @@ def image_create(request):
 
 def image_detail(request ,id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
-    return render(request, 'images/image/detail.html',{'section':'images','image':image})
+    total_views = r.incr(f'image:{image.id}:views')
+    return render(request, 'images/image/detail.html',{'section':'images','image':image, 'total_views':total_views})
 
 @login_required
 @require_POST
