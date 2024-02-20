da2852f0a4c48c7d581795d0d4e744a735125784
kadhir <kadhir@kadhir.com>
2024-02-20 18:27:09 +0530

    Remove registration Implementation

--- account/urls.py
+++ account/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='pasword_reest_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    path('register/', views.register, name='register'),
+    
 ]
