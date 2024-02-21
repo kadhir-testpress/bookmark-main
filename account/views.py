38ead4403531a36a95f1e548eaed1afe04cd7260
kadhir <kadhir@example.com>
2024-02-21 10:32:45 +0530

    removed register impementation

--- account/views.py
+++ account/views.py
@@ -2,8 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-from .forms import LoginForm, UserRegistrationForm
-from .models import Profile
+from .forms import LoginForm
 
 def user_login(request):
     if request.method == 'POST':
